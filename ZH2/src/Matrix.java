e01ec5bbc7f0ceed249930c04920839fe15a3e63
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-17 09:09:36 +0100

    Added try-with-resources block, replaced StringBuilder with StringJoiner

--- ZH2/src/Matrix.java
+++ ZH2/src/Matrix.java
@@ -1,49 +1,51 @@
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Matrix {
 
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    int N = s.nextInt();
-    int M = s.nextInt();
+    try (Scanner s = new Scanner(System.in)) {
+      int N = s.nextInt();
+      int M = s.nextInt();
 
-    int[][] mat = new int[N][M];
+      int[][] mat = new int[N][M];
 
-    for (int row = 0; row < N; row++) {
-      for (int col = 0; col < M; col++) {
-        mat[row][col] = s.nextInt();
-      }
-    }
-
-    s.close();
-
-    int positives[] = new int[M];
-
-    for (int col = 0; col < M; col++) {
       for (int row = 0; row < N; row++) {
-        if (mat[row][col] > 0) {
-          positives[col]++;
+        for (int col = 0; col < M; col++) {
+          mat[row][col] = s.nextInt();
         }
       }
-    }
 
-    int minPositive = positives[0];
+      s.nextLine();
 
-    for (int i = 0; i < M; i++) {
-      if (positives[i] < minPositive) {
-        minPositive = positives[i];
+      int positives[] = new int[M];
+
+      for (int col = 0; col < M; col++) {
+        for (int row = 0; row < N; row++) {
+          if (mat[row][col] > 0) {
+            positives[col]++;
+          }
+        }
       }
-    }
 
-    StringBuilder sb = new StringBuilder();
+      int minPositive = positives[0];
 
-    for (int i = 0; i < M; i++) {
-      if (positives[i] == minPositive) {
-        sb.append(i + 1).append(" ");
+      for (int i = 0; i < M; i++) {
+        if (positives[i] < minPositive) {
+          minPositive = positives[i];
+        }
       }
-    }
 
-    System.out.println(sb.toString().trim());
+      StringJoiner sj = new StringJoiner(" ");
+
+      for (int i = 0; i < M; i++) {
+        if (positives[i] == minPositive) {
+          sj.add(String.valueOf(i + 1));
+        }
+      }
+
+      System.out.println(sj);
+    }
   }
 
 }
