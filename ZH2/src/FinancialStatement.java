e8236747936488fb657a087a128d0eae0d7d5ebc
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-17 09:16:40 +0100

    Added try-with-resources block, simplify

--- ZH2/src/FinancialStatement.java
+++ ZH2/src/FinancialStatement.java
@@ -4,48 +4,47 @@ import java.util.Scanner;
 public class FinancialStatement {
 
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
+    try (Scanner s = new Scanner(System.in)) {
 
-    int cities = s.nextInt();
-    s.nextLine();
+      int cities = s.nextInt();
+      s.nextLine();
 
-    String[] lines = new String[cities];
+      String[] lines = new String[cities];
 
-    for (int i = 0; i < cities; i++) {
-      lines[i] = s.nextLine();
-    }
-
-    s.close();
-
-    double sum = 0;
-    double avg = 0;
-    int numOfValues = 0;
-    int na = 0;
-
-    for (int i = 0; i < cities; i++) {
-      String junk = String.format(lines[i].split(":")[1].trim());
-
-      if (isNumeric(junk)) {
-        sum += Double.parseDouble(junk);
-        numOfValues++;
-      } else {
-        na++;
+      for (int i = 0; i < cities; i++) {
+        lines[i] = s.nextLine();
       }
+
+      double sum = 0;
+      double avg = 0;
+      int numOfValues = 0;
+      int na = 0;
+
+      for (int i = 0; i < cities; i++) {
+        String junk = lines[i].split(":")[1].trim();
+
+        if (isNumeric(junk)) {
+          sum += Double.parseDouble(junk);
+          numOfValues++;
+        } else {
+          na++;
+        }
+      }
+
+      if (numOfValues != 0) {
+        avg = sum / numOfValues;
+      }
+
+      System.out.println(String.format(Locale.US, "Sum: %.2f", sum));
+
+      if (avg == 0) {
+        System.out.println("Avg: N/A");
+      } else {
+        System.out.println(String.format(Locale.US, "Avg: %.2f", avg));
+      }
+
+      System.out.println("N/A: " + String.valueOf(na));
     }
-
-    if (numOfValues != 0) {
-      avg = sum / numOfValues;
-    }
-
-    System.out.println(String.format(Locale.US, "Sum: %.2f", sum));
-
-    if (avg == 0) {
-      System.out.println("Avg: N/A");
-    } else {
-      System.out.println(String.format(Locale.US, "Avg: %.2f", avg));
-    }
-
-    System.out.println("N/A: " + String.valueOf(na));
 
   }
 
