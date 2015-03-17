c545e43bab2f78bc91388c126a0870f8055db706
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-17 09:30:29 +0100

    Added try-with-resources block

--- ZH3/src/Matrix.java
+++ ZH3/src/Matrix.java
@@ -3,42 +3,41 @@ import java.util.Scanner;
 public class Matrix {
 
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    int N = s.nextInt();
-    int M = s.nextInt();
+    try (Scanner s = new Scanner(System.in)) {
+      int N = s.nextInt();
+      int M = s.nextInt();
 
-    int[][] mat = new int[N][M];
+      int[][] mat = new int[N][M];
 
-    for (int row = 0; row < N; row++) {
-      for (int col = 0; col < M; col++) {
-        mat[row][col] = s.nextInt();
-      }
-    }
-
-    s.close();
-
-    int positives[] = new int[M];
-    int negatives[] = new int[M];
-
-    for (int col = 0; col < M; col++) {
       for (int row = 0; row < N; row++) {
-        if (mat[row][col] > 0) {
-          positives[col]++;
-        } else if (mat[row][col] < 0) {
-          negatives[col]++;
+        for (int col = 0; col < M; col++) {
+          mat[row][col] = s.nextInt();
         }
       }
-    }
 
-    StringBuilder sb = new StringBuilder();
+      int positives[] = new int[M];
+      int negatives[] = new int[M];
 
-    for (int i = 0; i < M; i++) {
-      if (positives[i] > negatives[i]) {
-        sb.append(i + 1).append(" ");
+      for (int col = 0; col < M; col++) {
+        for (int row = 0; row < N; row++) {
+          if (mat[row][col] > 0) {
+            positives[col]++;
+          } else if (mat[row][col] < 0) {
+            negatives[col]++;
+          }
+        }
       }
-    }
 
-    System.out.println(sb.toString().trim());
+      StringBuilder sb = new StringBuilder();
+
+      for (int i = 0; i < M; i++) {
+        if (positives[i] > negatives[i]) {
+          sb.append(i + 1).append(" ");
+        }
+      }
+
+      System.out.println(sb.toString().trim());
+    }
   }
 
 }
